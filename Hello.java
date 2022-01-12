9527b935dcc293429d6bb7f535290e95fd11f845
PranavBala1920 <81830566+PranavBala1920@users.noreply.github.com>
2022-01-12 17:54:01 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,8 +1,10 @@
 public class Hello {
 
 	public static void main(String[] args) {
-	System.out.println("Hello, World!");
-        Mainframe frame = new Mainframe();
-        frame.init();
+	    System.out.println("Hello, World!");
+            Mainframe frame = new Mainframe();
+            frame.init();
+		
     }
+	
 }
